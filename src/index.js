cc3d93167d95338bd8b2f3c11a9d8873a652541b
artemanoh <61391756+artemanoh@users.noreply.github.com>
2025-06-05 20:45:06 +0300

    make all

--- src/index.js
+++ src/index.js
@@ -28,9 +28,7 @@ class CountdownTimer {
     }
 
     const days = Math.floor(time / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(
-      (time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((time % (1000 * 60)) / 1000);
 
